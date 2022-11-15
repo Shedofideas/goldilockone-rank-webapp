21be7ffd0de9e8a47750d6348a5503ab8671ea2d
Eric Pereira <eric.pereira@hotmail.com>
2022-11-15 13:06:08 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import DataTable from 'react-data-table-component';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Box, Container, FormControl, InputLabel, MenuItem, Select, Typography } from '@mui/material';
+
+
+const columns = [
+  {
+    name: 'Position',
+    selector: row => row.Position,
+  },
+  {
+      name: 'Username',
+      selector: row => row.Username,
+  },
+  {
+      name: 'Difficulty',
+      selector: row => row.Difficulty,
+  },
+  {
+    name: 'PlayerType',
+    selector: row => row.PlayerType,
+  },
+  {
+    name: 'DamageReceived',
+    selector: row => row.DamageReceived,
+    sortable: true,
+  },
+  {
+    name: 'Duration',
+    selector: row => row.Duration,
+    sortable: true,
+  },
+];
 
 function App() {
+  const [rank, setRank] = useState()
+  const [difficulty, setDifficulty] = useState('Easy');
+
+  const handleChange = (event) => {
+    setDifficulty(event.target.value);
+  };
+
+  useEffect(() => {
+    axios.get(`https://goldilockone-ranking-api.herokuapp.com/?difficulty=${difficulty}`)
+      .then(function (response) {
+        setRank(response.data)
+      })
+      .catch(function (error) {
+        // handle error
+        //console.log(error);
+      })
+  }, [difficulty])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="md" style={{ paddingTop: 20 }}>
+      <Typography variant="h3" gutterBottom>
+        Goldilock One Ranking
+      </Typography>
+      <Box sx={{ width: 120 }}>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Difficulty</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={difficulty}
+            label="Difficulty"
+            onChange={handleChange}
+          >
+            <MenuItem value='Easy'>Easy</MenuItem>
+            <MenuItem value='Normal'>Normal</MenuItem>
+            <MenuItem value='Hard'>Hard</MenuItem>
+            <MenuItem value='Very Hard'>Very Hard</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+      <DataTable
+        columns={columns}
+        data={rank}
+      />
+    </Container>
   );
 }
 
